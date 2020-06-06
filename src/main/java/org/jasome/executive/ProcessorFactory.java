30eee10174829b1a05fc1c16a18ab7cdcd7b1851
amir9979 <amir9979@gmail.com>
2020-06-07 00:58:30 +0100

    Update ProcessorFactory.java

--- src/main/java/org/jasome/executive/ProcessorFactory.java
+++ src/main/java/org/jasome/executive/ProcessorFactory.java
@@ -22,11 +22,11 @@ class ProcessorFactory {
         processor.registerMethodCalculator(new NumberOfParametersCalculator());
         processor.registerPackageCalculator(new NumberOfClassesCalculator());
 
-//        processor.registerTypeCalculator(new SpecializationIndexCalculator());
+        processor.registerTypeCalculator(new SpecializationIndexCalculator());
 
 //        processor.registerPackageCalculator(new RobertMartinCouplingCalculator());
 
-//        processor.registerMethodCalculator(new NestedBlockDepthCalculator());
+        processor.registerMethodCalculator(new NestedBlockDepthCalculator());
 //        processor.registerTypeCalculator(new LackOfCohesionMethodsCalculator());
 
 //        processor.registerTypeCalculator(new ClassInheritanceCalculator());
