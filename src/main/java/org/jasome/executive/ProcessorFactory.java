07fbf53e7c5faac8c599c4d8009cab9022d77141
amir9979 <amir9979@gmail.com>
2020-06-07 00:47:53 +0100

    Update ProcessorFactory.java

--- src/main/java/org/jasome/executive/ProcessorFactory.java
+++ src/main/java/org/jasome/executive/ProcessorFactory.java
@@ -8,16 +8,16 @@ class ProcessorFactory {
         Processor processor = new Processor();
 
         processor.registerTypeCalculator(new RawTotalLinesOfCodeCalculator());
-//
-//        processor.registerTypeCalculator(new NumberOfFieldsCalculator());
-//
-//        processor.registerProjectCalculator(new TotalLinesOfCodeCalculator.ProjectCalculator());
-//        processor.registerPackageCalculator(new TotalLinesOfCodeCalculator.PackageCalculator());
-//        processor.registerTypeCalculator(new TotalLinesOfCodeCalculator.TypeCalculator());
-//        processor.registerMethodCalculator(new TotalLinesOfCodeCalculator.MethodCalculator());
-//
-//        processor.registerMethodCalculator(new CyclomaticComplexityCalculator());
-//        processor.registerTypeCalculator(new WeightedMethodsCalculator());
+
+        processor.registerTypeCalculator(new NumberOfFieldsCalculator());
+
+        processor.registerProjectCalculator(new TotalLinesOfCodeCalculator.ProjectCalculator());
+        processor.registerPackageCalculator(new TotalLinesOfCodeCalculator.PackageCalculator());
+        processor.registerTypeCalculator(new TotalLinesOfCodeCalculator.TypeCalculator());
+        processor.registerMethodCalculator(new TotalLinesOfCodeCalculator.MethodCalculator());
+
+        processor.registerMethodCalculator(new CyclomaticComplexityCalculator());
+        processor.registerTypeCalculator(new WeightedMethodsCalculator());
 //
 //        processor.registerMethodCalculator(new NumberOfParametersCalculator());
 //        processor.registerPackageCalculator(new NumberOfClassesCalculator());
