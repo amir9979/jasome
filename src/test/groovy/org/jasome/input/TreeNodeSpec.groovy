c285de3cf6624e0546c0c9d3dfab6184c6c45752
amir9979 <amir9979@gmail.com>
2020-06-07 11:30:50 +0100

    Update TreeNodeSpec.groovy

--- src/test/groovy/org/jasome/input/TreeNodeSpec.groovy
+++ src/test/groovy/org/jasome/input/TreeNodeSpec.groovy
@@ -13,15 +13,15 @@ class TreeNodeSpec extends Specification {
         int left = 2
         int right = 2
 
-//        def project = projectFromSnippet('''
-//        package org.whatever.stuff1;
-//
-//        class Thing1 {
-//            public String toString() {
-//                return "thing";
-//            }
-//        }
-//        ''')
+        Project project = projectFromSnippet('''
+        package org.whatever.stuff1;
+
+        class Thing1 {
+            public String toString() {
+                return "thing";
+            }
+        }
+        ''')
 
         when:
         int result = left + right
